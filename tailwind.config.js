fdfc5ca7d0a36ac9482e3bdd69f9809fad2db983
Sander <hosin8388@gmail.com>
2026-09-13 19:44:57 +0330

    finishing with Dark&Light mode

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // 👈 این خط مهم است
+  content: [
+    "./**/*.html",
+    "./**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
