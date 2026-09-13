8284f2514dc61b70114bfde16f1c10e177bef815
Sander <hosin8388@gmail.com>
2026-09-13 23:09:34 +0330

    edited some info in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/exports-car-accessories/', // این خط را دقیقاً با نام مخزن خود اضافه کنید
   plugins: [
     tailwindcss(),
   ],
