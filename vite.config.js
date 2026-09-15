b2c39d67625229dd627e29afd6071c8a99c4f2b4
Saeid Rezaei <saeid.rezaei695@gmail.com>
2026-09-15 14:24:13 +0330

    develop by saeid

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-
+//حسین جان فیکس شد
   base: './',
   plugins: [
     tailwindcss(),
